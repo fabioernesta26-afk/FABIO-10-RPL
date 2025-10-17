e634d81bbd0ebe6fbd3df6517f1d61d21a307f60
fabioernesta26-afk <fabioernesta26@gmail.com>
2025-10-17 21:00:18 +0700

    Add files via upload

---
+++ KELAS X-RPL/KELAS/9 OKTOBER 2025/index.php
@@ -0,0 +1,84 @@
+belajar php
+<h1>saya belajar php</h1>
+
+ <?php 
+$nama = "Nur Iman Pambudi Luhur";
+$kelas = 12;
+$umur = 16;
+$alamat = "taman pinang asri";
+$hobi = "ngedit,ngoding,ngocok";
+$cita = "editor";
+
+echo $nama;
+echo $enter;
+
+echo $kelas;
+echo $enter;
+
+echo $umur;
+echo $enter;
+
+echo $alamat;
+echo $enter;
+
+echo $hobi;
+echo $enter;
+
+echo $cita;
+echo $enter;
+
+ /*   echo "<h1>saya belajar php";
+    echo "saya kelas:";
+    echo 12;
+
+
+    echo "nama:<br>";
+    echo "Fabio Ernesta Hariyanto<br>";
+
+
+    echo "kelas: <br>";
+    echo 10;
+    echo "<br>";
+
+    echo "Saya umur: <br>";
+    echo 16;
+    echo "<br>";
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>web Iman</title>
+</head>
+<body>
+    <div>
+        <h1>identitas</h1>
+    </div>
+    <table>
+        <tbody>
+        <tr>
+            <td>nama</td>
+        </tr>
+        <tr>
+            <td>kelas</td>
+        </tr>
+        <tr>
+            <td>umur</td>
+        </tr>
+        <tr>
+            <td>alamat</td>
+        </tr>
+        <tr>
+            <td>hobi</td>
+        </tr>
+        <tr>
+            <td>cita-cita</td>
+        </tr>
+        </tbody>
+    </table>
+</body>
+</html>
