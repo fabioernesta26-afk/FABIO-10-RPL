e8582deb1a394587fad3b79d10aa64bd31fedf65
fabioernesta26-afk <fabioernesta26@gmail.com>
2025-10-19 11:42:01 +0700

    Add files via upload

---
+++ KELAS X-RPL/VIDEO/PHP/EPS 1-6(Latihan)/lat01.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Belajar PHP";
+
+echo '<br>';
+
+echo 'Saya siswa SMK';
+
+echo '<h1 style="background-color:red">Belajar PHP itu mudah</h1>
+      <p <h1 style="background-color:blue">Saya suka belajar program</p>  
+';
+
+echo 'Saya belajar' . 'PHP' . '<br>';
+
+echo 2025 + 5;
+
+?>
