99bfa4a6b2e9020920f3f9563965c8996a283d22
fabioernesta26-afk <fabioernesta26@gmail.com>
2026-01-20 14:11:18 +0700

    input output

---
+++ KELAS X-RPL/KELAS 2/tugas pak tri/20-01-2026/src/belajarkonversidata.java
@@ -0,0 +1,15 @@
+
+public class belajarkonversidata {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Belajar konversi data");
+        System.out.println("");
+        
+        String a = "25";
+//        int b = Integer.valueOf(a);
+          int c = Integer.parseInt(a);
+        System.out.println(c + 100);
+    }
+    
+}
